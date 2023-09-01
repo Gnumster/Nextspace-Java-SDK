3ca44b9a6b1f9ea3f9a02a4a7c98188beb44d074
AlexL <alex.lavrinovich@nextspace.com>
2023-09-01 13:51:37 +1200

    Initial state of the project without much in it.

---
+++ JavaConsoleNextspaceClientApplicationExample/src/main/java/org/nextspace/javaconsolenextspaceclientapplicationexample/JavaConsoleNextspaceClientApplicationExample.java
@@ -0,0 +1,15 @@
+package org.nextspace.javaconsolenextspaceclientapplicationexample;
+
+/**
+ * This Console application is provided as an example of interaction with Nextspace platform via APIs.
+ * It conducts a series of requests, illustrating the use of the Nextspace Java Client library.
+ * @author alex.l
+ */
+public class JavaConsoleNextspaceClientApplicationExample
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println(">> JavaConsoleNextspaceClientApplicationExample ver.1.0.1 has started.");
+    }
+}
