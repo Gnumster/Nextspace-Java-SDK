27ab0e4262bb15d5ebc259ef1662f5069a8870bd
AlexL <alex.lavrinovich@nextspace.com>
2023-09-13 12:55:39 +1200

    Added some comments.

--- JavaConsoleNextspaceClientApplicationExample/src/main/java/org/nextspace/javaconsolenextspaceclientapplicationexample/JavaConsoleNextspaceClientApplicationExample.java
+++ JavaConsoleNextspaceClientApplicationExample/src/main/java/org/nextspace/javaconsolenextspaceclientapplicationexample/JavaConsoleNextspaceClientApplicationExample.java
@@ -12,27 +12,76 @@ import java.util.List;
  */
 public class JavaConsoleNextspaceClientApplicationExample
 {
-
     public static void main(String[] args)
     {
         System.out.println(">> JavaConsoleNextspaceClientApplicationExample ver.1.0.1 has started.");
         
         try
         {
-            Client client = new Client(NextspaceHostingEnvironment.UAT, "alex", null, null);
+            /*
+             * =============================================================================
+             *
+             * Interaction with Nextspace API (aka "Bruce API") on the client side
+             * begins with creating a new instance of the Client() object,
+             * supplyhing it with the key parameters:
+             * 
+             *  - Environment in which the operation takes place: PROD, UAT, STG or DEV.
+             *  - Client Account identified by its Subdomain.
+             *  - User Login 
+             *  - User Password (not saved anywhere in the code)
+             *
+             *  User Login and Password are optional. If no login and password is supplied,
+             *  Client() is initialized to act on behalf of Anonymous user, which will
+             *  have limited Access Permission. Some of the calls made using this option
+             *  will fail with "Access Denied" exception.
+             *
+             * =============================================================================
+             */
+            
+            Client client = new Client(NextspaceHostingEnvironment.PROD, "alex", null, null);
             
             String bruceAPIURL = client.getBruceAPIURL();
         
             System.out.println(">Bruce API URL : " + bruceAPIURL);
             
+            /*
+             * =============================================================================
+             *
+             * Following is how you can get the list of all Entity Types registered on
+             * Client Account for which Client() is initialized.
+             *
+             * =============================================================================
+             */
+            
             List<EntityType> list = client.GetEntityTypes();
             
-            System.out.println(">Entity Types: ");
+            EntityType SelectedEntityType = null;
+            
+            System.out.println(">Entity Types (first 20): ");
+            
+            int count = 0;
             
             for (EntityType et : list)
             {
                 System.out.println("- ID=" + et.ID + ", Name=" + et.Name);
+                
+                if (SelectedEntityType==null) SelectedEntityType = et; // Selecting one Entity Type for the next action.
+                
+                if (count++ >= 20) break;
             }
+            
+            /*
+             * =============================================================================
+             *
+             * Following is how you can get details of one Entity Type.
+             *
+             * =============================================================================
+             */
+            
+            EntityType t = EntityType.GetByID(client, SelectedEntityType.ID);
+            
+            System.out.println(">Entity Type Details: " + t.ID + " received." );
+            
             
         }
         catch (Exception up)
